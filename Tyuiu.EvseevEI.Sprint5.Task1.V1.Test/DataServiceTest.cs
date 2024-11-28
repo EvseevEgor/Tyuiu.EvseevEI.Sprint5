718cc10bed970e8c191afb0b3e85e25614d3f527
Евсеев Егор <Tyuiu.EvseevEI@outlook.com>
2024-11-28 21:37:51 +0500

    1

---
+++ Tyuiu.EvseevEI.Sprint5.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.EvseevEI.Sprint5.Task1.V1.Lib;
+namespace Tyuiu.EvseevEI.Sprint5.Task1.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\Evseevegor709icloud\source\repos\Tyuiu.EvseevEI.Sprint5\Tyuiu.EvseevEI.Sprint5.Task0.V19\bin\Debug\OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.EvseevEI.Sprint5.Task1.V1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.EvseevEI.Sprint5.Task1.V1.Lib;
+namespace Tyuiu.EvseevEI.Sprint5.Task1.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startValue = -5;
+            int stopValue = 5;
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Евсеев Е.И | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Евсеев Егор Игоревич | ИПБ-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Произвести табулирование функции на диапазоне от -5 до 5 с шагом 1.     *");
+            Console.WriteLine("* При делении на 0 вернуть значение 0. Результат сохранить в текстовый    *");
+            Console.WriteLine("* файл и вывести на консоль в таблицу.                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
